37ffe2bad54f35ac56792bfcd8ebe180694fd3dd
Daniel <dhcgn@users.noreply.github.com>
2025-12-24 00:00:28 +0100

    Bump version code to 2 and version name to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "app.hdev.io.aitranscribe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
